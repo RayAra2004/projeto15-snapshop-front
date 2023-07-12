3ee70da5dba9dbaecac4ffe1f0c54b66306e8787
Schwartz <darlanswtz@gmail.com>
2023-07-12 18:30:37 -0300

    Style: Header routes and navbar routes with styles and some logic implemented

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -2,26 +2,41 @@ import styled from "styled-components";
 import { mainColor } from "../Colors/colors.js";
 import favicon from "../../public/favicon.png"
 import { BsSearch } from 'react-icons/bs';
+import { useLocation, useNavigate } from "react-router";
+import banner from '../../public/banner.png'
+import { useRef } from "react";
 
 export default function Header() {
+
+    const navigate = useNavigate();
+    const location = useLocation();
+    const searchRef = useRef();
+ 
+    function search()
+    {
+        console.log(searchRef.current.value);
+    }
+
     return (
 
         <HeaderContainer>
-            <Logo>
-                <img className="favicon" src={favicon} alt="" />
-                <h1>SnapShop</h1>
-            </Logo>
+            <div className="content">
+                <Logo onClick={() => navigate('/')}>
+                    <img className="favicon" src={favicon} alt="" />
+                    <h1>SnapShop</h1>
+                </Logo>
 
-            <SearchBar>
-                <input type="text" placeholder="Buscar produtos, marcas e muito mais…" />
-                <BsSearch className="search-icon" />
-            </SearchBar>
+                {location.pathname !== '/cadastro' && location.pathname !== '/login' &&
 
-            <div className="banner">
-                ANUNCIO
+                    <SearchBar>
+                        <input ref={searchRef} type="text" placeholder="Buscar produtos, marcas e muito mais…" />
+                        <BsSearch onClick={search} className="search-icon" />
+                    </SearchBar>
+                }
+
+                    <img className="banner" src={banner} title="Loyalty" alt="Loyalty" />
             </div>
         </HeaderContainer>
-
     );
 }
 
@@ -34,11 +49,23 @@ const HeaderContainer = styled.header`
     left: 0;
     top: 0;
     display: flex;
-    justify-content: space-between;
+    justify-content: center;
     align-items: center;
+
+    .content{
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+        width: 100%;
+        max-width: 1200px;
+    }
 
     .banner{
         margin-right: 20px;
+        width: 300px;
+        height: 40px;
+        cursor: pointer;
+        
     }
 `;
 
--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,49 +1,91 @@
 import styled from "styled-components";
 import { mainColor } from "../Colors/colors.js";
-import favicon from "../../public/favicon.png"
-import { BsSearch, BsFillBellFill ,BsFillCartFill} from 'react-icons/bs';
-import { useNavigate } from "react-router";
+import { BsFillBellFill, BsFillCartFill, BsPersonCircle, BsFillCaretDownFill } from 'react-icons/bs';
+import { useLocation, useNavigate } from "react-router";
 import { useState } from "react";
+import Swal from "sweetalert2";
+import logoutIcon from '/logout.svg';
 
 export default function Navbar() {
 
     const navigate = useNavigate();
-    const [showUserInfo,setShowUserInfo] = useState(false);
+    const [showUserInfo, setShowUserInfo] = useState(false);
+    const location = useLocation();
+
+    function logout() {
+        Swal.fire({
+            title: `<span style="font-family: 'Mulish', sans-serif;font-size: 20px;color:black">Deseja sair?</span>`,
+            showCancelButton: true,
+            confirmButtonColor: '#c9c9c9',
+            cancelButtonColor: `${mainColor}`,
+            confirmButtonText: 'Sim',
+            cancelButtonText: 'Cancelar',
+            width: 300,
+            heightAuto: false,
+            imageUrl: logoutIcon,
+            imageWidth: 200,
+            imageHeight: 100,
+        }).then((result) => {
+            if (result.isConfirmed) {
+                localStorage.removeItem('token');
+                navigate('/');
+            }
+        });
+    }
 
     return (
 
-        <NavContainer>
-            <div className="actions">
-                <Navbutton onClick={() => navigate('#')}>
-                    <p>Categorias</p>
-                </Navbutton>
-                <Navbutton onClick={() => navigate('#')}>
-                    <p>Histórico</p>
-                </Navbutton>
-                <Navbutton onClick={() => navigate('/adicionar-produto')}>
-                    <p>Vender</p>
-                </Navbutton>
-                <Navbutton onClick={() => navigate('#')}>
-                    <p>Contato</p>
-                </Navbutton>
-            </div>
+        <>
+            {location.pathname !== '/cadastro' && location.pathname !== '/login' && 
 
-            <div className="user-actions">
-                <Navbutton onMouseEnter={()=> setShowUserInfo(true)} onMouseLeave={()=> setShowUserInfo(false)}>
-                    <p>Usuário</p>
-                    
-                </Navbutton>
-                <Navbutton onClick={() => navigate('/minhas-compras')}>
-                    <p>Compras</p>
-                </Navbutton>
-                <Navbutton onClick={() => navigate('#')}>
-                    <p>Favoritos</p>
-                </Navbutton>
-                <BsFillBellFill onClick={() => navigate('#')} className="notifications-btn"/>
-                <BsFillCartFill onClick={()=> navigate('/carrinho')} className="cart-btn"/>
-            </div>
-        </NavContainer>
 
+                <NavContainer onMouseEnter={() => setShowUserInfo(false)} onMouseLeave={() => setShowUserInfo(false)}>
+                    <div className="content">
+                    <div className="actions">
+                        <Navbutton onClick={() => navigate('#')}>
+                            <p>Categorias</p>
+                        </Navbutton>
+                        <Navbutton onClick={() => navigate('#')}>
+                            <p>Histórico</p>
+                        </Navbutton>
+                        <Navbutton onClick={() => navigate('/meus-produtos')}>
+                            <p>Vender</p>
+                        </Navbutton>
+                        <Navbutton onClick={() => navigate('#')}>
+                            <p>Contato</p>
+                        </Navbutton>
+                    </div>
+
+                    <div className="user-actions">
+                        <Navbutton onMouseEnter={() => setShowUserInfo(true)} >
+                            <BsPersonCircle />
+                            <p className="user">Usuário <BsFillCaretDownFill className="drop" /></p>
+                            {showUserInfo &&
+                                <div onMouseLeave={() => setShowUserInfo(false)} className="user-info">
+                                    <div className="btn" onClick={() => navigate('/editar-usuario')}>Editar informações</div>
+                                    <div className="btn" onClick={() => navigate('/minhas-compras')}>Compras</div>
+                                    <div className="btn" onClick={() => navigate('/meus-produtos')}>Vender</div>
+                                    <div className="btn" onClick={logout}>Sair</div>
+                                </div>
+                            }
+
+                        </Navbutton>
+                        <Navbutton onClick={() => navigate('/minhas-compras')}>
+                            <p>Compras</p>
+                        </Navbutton>
+                        <Navbutton onClick={() => navigate('#')}>
+                            <p>Favoritos</p>
+                        </Navbutton>
+                        <BsFillBellFill onClick={() => navigate('#')} className="notifications-btn" />
+                        <div className="cart">
+                            <BsFillCartFill onClick={() => navigate('/carrinho')} className="cart-btn" />
+                            <p className="cart-items-amount">10</p>
+                        </div>
+                    </div>
+                    </div>
+                </NavContainer>
+            }
+        </>
     );
 }
 
@@ -56,23 +98,53 @@ const NavContainer = styled.header`
     left: 0;
     top: 80px;
     display: flex;
-    justify-content: space-evenly;
+    justify-content: center;
     align-items: center;
 
-    .actions,.user-actions{
+    .content{
+        width: 100%;
+        max-width: 1200px;
         display: flex;
         justify-content: space-between;
         align-items: center;
-    }
+        .actions,.user-actions{
+            display: flex;
+            justify-content: space-between;
+            align-items: center;
+            gap: 20px;
+        }
 
     .user-actions{
 
         .notifications-btn,.cart-btn{
             color: white;
-            margin-left: 20px;
+            transition: all 200ms;
             cursor: pointer;
+            &:hover{
+                color: lightgray;
+            }
         }
+
+        .cart{
+            position: relative;
+            justify-content: center;
+            display: flex;
+            align-items: center;
+            .cart-items-amount{
+                position: absolute;
+                right: 3px;
+                top: 3px;
+                font-size: 10px;
+                font-weight: bold;
+                pointer-events: none;
+            }
     }
+    }
+
+    }
+
+    
+    
 `;
 
 const Navbutton = styled.button`
@@ -80,16 +152,61 @@ const Navbutton = styled.button`
     color: white;
     border: 0;
     background:none;
+    position: relative;
     cursor: pointer;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    gap: 10px;
+    transition: all 200ms;
 
     &:hover{
         color: lightgray;
     }
 
+    .user{
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        .drop{
+            padding-top: 5px;
+            padding-left: 5px;
+            font-size:10px;
+        }
+    }
+
     .user-info{
         width: 200px;
-        height: 300px;
-        background-color: red;
+        padding-bottom: 20px;
+        background-color: white;
+        border-bottom-left-radius: 5px;
+        border-bottom-right-radius: 5px;
+        position: absolute;
+        top: 29px;
+        transition: all 200ms;
+        left: 0;
+        box-shadow: 2px 2px 2px rgba(0, 0, 0, 0.1), -3px 0px 3px rgba(0, 0, 0, 0.1);
+
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        flex-direction: column;
+
+        .btn{
+            color: black;
+            width: 100%;
+            height: 40px;
+            background-color: white;
+            border: 0;
+            display: flex;
+            align-items: center;
+            justify-content: center;
+            cursor: pointer;
+            transition: all 200ms;
+            &:hover{
+                background-color: #f0f0f0;
+            }
+        }
     }
 `;
 
