e5a203fd11138861bc6288679b6c9b458b1b3c1a
Schwartz <darlanswtz@gmail.com>
2023-07-16 01:38:12 -0300

    Style: fixed logic and style for compost purchases

--- src/pages/MyPurchases.jsx
+++ src/pages/MyPurchases.jsx
@@ -30,33 +30,78 @@ export default function MyPurchases() {
         axios.get(`${import.meta.env.VITE_API_URL}/minhas-compras`, config)
             .then(res => {
                 setProducts(res.data);
+                console.log(res.data);
             })
             .catch(res => console.log(res));
     }, []);
+
+    function somarMatriz(matriz) {
+        let soma = 0;
+        for (let i = 0; i < matriz.length; i++) {
+          soma += matriz[i];
+        }
+        return soma;
+      }
 
     function renderProducts() {
         if (products === undefined) return <p>Carregando...</p>
         if (products && products.length == 0) return <p className="no-purchases">Você não possui compras</p>
         return (
             products.map(product => {
-                if (product.name !== undefined) {
-                    return (
-                        <Purchase key={uuidv4()}>
-                            <div className="main">
-                                <div className="image-and-name">
-                                    <img src={product.picture} alt={product.name} />
-                                    <p>{product.name.substring(0, 10).trim()}{product.name.length > 10 ? '...' : ''}</p>
+                if (product.buys !== undefined) {
+                    if(product.buys.length == 1)
+                    {
+                        return (
+                            <Purchase key={uuidv4()}>
+                                <div className="main">
+                                    <div className="image-and-name">
+                                        <img src={product.buys[0].picture} alt={product.name} />
+                                        <p>{product.buys[0].name.substring(0, 10).trim()}{product.buys[0].name.length > 10 ? '...' : ''}</p>
+                                    </div>
+                                    <div className="numbers">
+                                        <span>R$ {String(Number(product.info.price ? product.info.price : product.value).toFixed(2)).replace('.', ',')}</span>
+                                        <p className="quant">Quantidade:{product.info.amount}</p>
+                                    </div>
                                 </div>
-                                <div className="numbers">
-                                    <span>R${String(Number(product.info.price ? product.info.price : product.value).toFixed(2)).replace('.', ',')}</span>
-                                    <p className="quant">Quantidade:{product.info.amount}</p>
+                                <div className="secondary">
+                                    <p className="adress"><BiSolidTruck className="truck" /> {`${product.info.street} ${product.info.number} - ${product.info.neighborhood} - ${product.info.city},${product.info.state}`.substring(0, 40)}...</p>
                                 </div>
-                            </div>
-                            <div className="secondary">
-                                <p className="adress"><BiSolidTruck className="truck" /> {`${product.info.street} ${product.info.number} - ${product.info.neighborhood} - ${product.info.city},${product.info.state}`.substring(0, 40)}...</p>
-                            </div>
-                        </Purchase>
-                    );
+                            </Purchase>
+                        );
+                    }
+                    else
+                    {
+                        const totalItems =  somarMatriz(product.info.amount);
+                        
+                        return (
+                            <CompostPurchase key={uuidv4()}>
+                                <div className="main">
+                                    <div className="image-and-name">
+                                        <img src={product.buys[0].picture} alt={product.name} />
+                                        <div className="names">
+                                            {
+                                                product.buys.map((b,index)=> {
+                                                    return (
+                                                        <p className="mini-name">
+                                                            {b.name.substring(0, 10).trim()}{b.name.length > 10 ? '...' : ''}
+                                                            <span className="mini-amount">{product.info.amount[index]}</span> 
+                                                        </p>
+                                                    );
+                                            })
+                                            }
+                                        </div>
+                                    </div>
+                                    <div className="numbers">
+                                        <span>R$ {String(Number(product.info.price ? product.info.price : product.value).toFixed(2)).replace('.', ',')}</span>
+                                        <p className="quant">Quantidade: {totalItems}</p>
+                                    </div>
+                                </div>
+                                <div className="secondary">
+                                    <p className="adress"><BiSolidTruck className="truck" /> {`${product.info.street} ${product.info.number} - ${product.info.neighborhood} - ${product.info.city},${product.info.state}`.substring(0, 40)}...</p>
+                                </div>
+                            </CompostPurchase>
+                        );
+                    }
                 }
                 else
                 {
@@ -128,6 +173,130 @@ const SCProducts = styled.div`
         white-space: nowrap;
     }
 `
+
+const CompostPurchase = styled.div`
+    background-color: white;
+    width: 100%;
+    max-width: 600px;
+    min-height: 120px;
+    border-radius: 10px;
+    padding: 15px;
+    box-sizing: border-box;
+    box-shadow: 2px 2px 2px rgba(0, 0, 0, 0.1);
+    display: flex;
+    flex-direction: column;
+    justify-content: flex-start;
+    transition: all 200ms;
+    *{
+        font-family: 'Mulish', sans-serif;
+        transition: all 200ms;
+    }
+    .secondary{
+        margin-top: 20px;
+        display: flex;
+        flex-direction: column;
+        width: 100%;
+        gap: 5px;
+        .adress{
+            font-size: 14px;
+            display: flex;
+            align-items: center;
+            gap: 5px;
+            line-break: anywhere;
+        }
+
+        .truck{
+            color: ${mainColor};
+        }
+    }
+
+    @media (max-width:583px) {
+       border-radius: 0;
+    }
+    .names{
+        display: flex;
+        width: fit-content;
+        gap: 10px;
+        flex-wrap: wrap;
+        .mini-name{
+            font-size: 10px;
+            
+        }
+        .mini-amount{
+            font-size: 10px;
+            margin-left: 3px;
+            color: ${mainColor};
+        }
+
+        @media (max-width:639px) {
+            max-width: 250px;
+        }
+
+        @media (max-width:518px) {
+            max-width: 200px;
+        }
+       
+    }
+
+    .numbers{
+        min-width: 85px;
+    }
+
+    .main{
+        display: flex;
+        align-items: center;
+        justify-content: space-between;
+        width: 100%;
+
+        .image-and-name{
+            display: flex;
+            align-items: center;
+            
+
+            img{
+                width: 70px;
+                aspect-ratio: 1;
+                object-fit: cover;
+                border-radius: 50%;
+                margin-right: 15px;
+                border: 1px solid ${secondaryColor};
+            }
+        }
+        .quant{
+            width: auto;
+            font-size: 12px;
+            text-align: right;
+            margin-top: 10px;
+            @media (max-width:441px) {
+            font-size: 10px;
+        }
+        }
+    }
+    p{
+        height: auto;
+        font-weight: 700;
+        font-size: 30px;
+        @media (max-width:459px) {
+            font-size: 20px;
+        }
+    }
+
+    span{
+        font-size: 25px;
+        font-weight: 600;
+        color: rgb(96 223 34);
+        margin-top: 10px;
+        text-align: right;
+        @media (max-width:469px) {
+            font-size: 20px;
+        }
+
+        @media (max-width:441px) {
+            font-size: 15px;
+        }
+    }
+
+`;
 
 const Purchase = styled.div`
     background-color: white;
